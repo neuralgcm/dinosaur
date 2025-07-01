6af1d7fb91380c283010fff7873a76c142d1e099
Stephan Hoyer <shoyer@google.com>
2025-06-30 17:59:27 -0700

    Fix license and source-code URLs in PyPI metadata
    "Apache-2.0" is the standard SPDX string which PyPI prefers.
    
    "source-code" is the right way to indicate a GitHub link.
    
    PiperOrigin-RevId: 777795945

--- dinosaur/__init__.py
+++ dinosaur/__init__.py
@@ -35,4 +35,4 @@ import dinosaur.vertical_interpolation
 import dinosaur.weatherbench_utils
 import dinosaur.xarray_utils
 
-__version__ = "1.3.3"  # keep sync with pyproject.toml
+__version__ = "1.3.4"  # keep sync with pyproject.toml
